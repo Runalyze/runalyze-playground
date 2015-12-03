e7075f9105fabe2ebe188d2beff9d76c92348153
laufhannes <mail@laufhannes.de>
2015-12-03 07:38:12 +0100

    minor improvements for graph example

--- feature/d3js/graph/data.php
+++ feature/d3js/graph/data.php
@@ -1,21 +1,27 @@
 <?php
-$db = new mysqli('localhost', 'root', '', 'runalyze');
 
-if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-}
+$LOAD_JS = false;
+$LOAD_HTML = false;
+require_once '../../../bootstrap.php';
 
-$result = $db->query('SELECT * FROM runalyze_training tr LEFT JOIN runalyze_trackdata ta ON tr.id=ta.activityid Limit 1');
-$data = $result->fetch_assoc();
+$PDO = DB::getInstance();
+
+$where = isset($_GET['id']) ? 'WHERE `activityid`='.(int)$_GET['id'] : '';
+
+$result = $PDO->query(
+	'SELECT * FROM `'.PREFIX.'trackdata` '.$where.' ORDER BY `activityid` DESC LIMIT 1'
+);
+
+$data = $result->fetch(PDO::FETCH_ASSOC);
 $data['cadence'] = explode('|', $data['cadence']);
 $data['distance'] = explode('|', $data['distance']);
 $data['time'] = explode('|', $data['time']);
 $data['groundcontact'] = explode('|', $data['groundcontact']);
 $data['groundcontact_balance'] = explode('|', $data['groundcontact_balance']);
 $data['vertical_oscillation'] = explode('|', $data['vertical_oscillation']);
-$data['vertical_ratio'] = explode('|', $data['vertical_ratio']);
 $data['temperature'] = explode('|', $data['temperature']);
 $data['heartrate'] = explode('|', $data['heartrate']);
+$data['power'] = explode('|', $data['power']);
 
 //print_r($data);
 echo json_encode($data);
