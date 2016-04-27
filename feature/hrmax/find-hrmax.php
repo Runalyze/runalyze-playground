fa8fefa796bf5f070d090b857d20a349f7796889
undertrained <undertrained@users.noreply.github.com>
2016-04-27 08:26:35 +0200

    allow for '?months=$n$', simplify SQL

--- feature/hrmax/find-hrmax.php
+++ feature/hrmax/find-hrmax.php
@@ -3,7 +3,7 @@ require_once '../../bootstrap.php';
 
 use Runalyze\Activity\Duration;
 
-$limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 1000;
+$months = isset($_GET['months']) ? (int)$_GET['months'] : 3;
 
 $sportStatement = DB::getInstance()->query(
 	'SELECT
@@ -13,7 +13,7 @@ $sportStatement = DB::getInstance()->query(
 );
 
 while ($s = $sportStatement->fetch()) {
-	$where = isset($_GET['id']) ? '`id`="'.(int)$_GET['id'].'"' : ('`s` > 1800 AND `sportid`='.$s['id'].' AND `act`.`time` >= UNIX_TIMESTAMP(CURRENT_DATE - INTERVAL 3 MONTH)');
+	$where = isset($_GET['id']) ? '`id`="'.(int)$_GET['id'].'"' : ('`s` > 1800 AND `sportid`='.$s['id'].' AND `act`.`time` >= UNIX_TIMESTAMP(CURRENT_DATE - INTERVAL '.$months.' MONTH)');
 
 	$Statement = DB::getInstance()->query(
 		'SELECT
@@ -29,22 +29,12 @@ while ($s = $sportStatement->fetch()) {
 		FROM `'.PREFIX.'training` AS `act`
 		LEFT JOIN `'.PREFIX.'trackdata` AS `track` ON `track`.`activityid` = `act`.`id`
 		WHERE '.$where.' AND `act`.`accountid` > -1 AND `track`.`heartrate` IS NOT NULL AND `track`.`heartrate` != ""
-		ORDER BY `timestamp` DESC
-		LIMIT '.$limit
+		ORDER BY `pulse_max` DESC
+		LIMIT 1'
 	);
 
-	$hrmax = 0;
-	$data = NULL;
-
-	while ($d = $Statement->fetch()) {
-		if ($d['pulse_max'] > $hrmax) {
-			$hrmax = $d['pulse_max'];
-			$data = $d;
-		}
-	}
-
-	if ($hrmax > 0) {
-		echo '<strong>'.$s['name'].': '.$hrmax.'</strong>';
+	if ($data = $Statement->fetch()) {
+		echo '<strong>'.$s['name'].': '.$data['pulse_max'].'</strong>';
 		echo ' ('.date('d.m.Y', $data['timestamp']).', '.$data['km'].'k in '.Duration::format($data['s']).' at &oslash; '.$data['pulse_avg'].'bpm)<br/>';
 	}
 }
