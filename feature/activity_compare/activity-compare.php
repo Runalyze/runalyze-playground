f2484fb0675a6660cadaf652b30f8e3959d8691d
grzeg1 <grzegorz@thulium.pl>
2016-04-27 23:01:40 +0200

    Compare activities feature

--- feature/activity_compare/activity-compare.php
+++ feature/activity_compare/activity-compare.php
@@ -9,7 +9,7 @@ use Runalyze\Model\Trackdata;
 use Runalyze\Parameter\Application\PaceUnit;
 
 $activityIDs = isset($_GET['activityIDs']) ? $_GET['activityIDs'] : "719,726";
-$adjustDistance = isset($_GET['adjustDistance']) ? boolval($_GET['adjustDistance']) : true;
+$adjustDistance = isset($_GET['adjustDistance']) ? $_GET['adjustDistance'] == 'yes' : true;
 
 $where = "`id` in ($activityIDs)";
 
@@ -33,58 +33,62 @@ $Statement = DB::getInstance()->query(
 	FROM `' . PREFIX . 'training` AS `act`
 	LEFT JOIN `' . PREFIX . 'trackdata` AS `track` ON `track`.`activityid` = `act`.`id`
 	WHERE ' . $where . ' AND `act`.`accountid` > -1 AND `track`.`heartrate` IS NOT NULL AND `track`.`heartrate` != ""
-	ORDER BY `timestamp`
+	ORDER BY FIELD(id,' . $activityIDs . ')
 ');
 
 $i = 1;
 
 while ($data = $Statement->fetch()) {
-    echo '<strong>' . date('d.m.Y', $data['timestamp']) . ', ' . $data['km'] . 'k in ' . Duration::format($data['s']) . ' at &oslash; ' . $data['pulse_avg'] . 'bpm = VDOT ' . $data['vdot'] . '</strong>';
-    echo '<br/>';
-
     $Trackdata[$i] = new Trackdata\Entity($data);
     $Loop[$i] = new Trackdata\Loop($Trackdata[$i]);
-    $distCoef[$i] = $adjustDistance ? $Trackdata[$i]->totalDistance() / $data['km'] : 1;
+    $totalTime[$i] = $adjustDistance ? $data['s'] : $Trackdata[$i]->totalTime();
+    $totalDistance[$i] = $adjustDistance ? $data['km'] : $Trackdata[$i]->totalDistance();
+    $distCoef[$i] = $Trackdata[$i]->totalDistance() / $totalDistance[$i];
+    $avgPace[$i] = (new Pace($totalTime[$i], $totalDistance[$i], PaceUnit::MIN_PER_KM))->value();
+    echo '<strong>' . date('d.m.Y', $data['timestamp']) . ', ' . $totalDistance[$i] . 'k in ' . Duration::format($totalTime[$i]) . ', ' . $data['pulse_avg'] . 'bpm = VDOT ' . $data['vdot'] . ' avg pace ' . $avgPace[$i] . '/km</strong>';
+    echo '<br/>';
+
     $i++;
 };
 
-$vRacerPace = isset($_GET['vRacerPace']) ? $_GET['vRacerPace'] : (new Pace($Trackdata[1]->totalTime(), $Trackdata[1]->totalDistance(), PaceUnit::MIN_PER_KM))->value();
+$vRacerPace = isset($_GET['vRacerPace']) ? $_GET['vRacerPace'] : $avgPace[1];
 
-$Loop[0] = new Trackdata\VirtualLoop($vRacerPace, $Trackdata[1]->totalTime());
+$Loop[0] = new Trackdata\VirtualLoop($vRacerPace, $totalTime[1]);
+$activityCount = sizeof($Trackdata);
 
 while (!($Loop[0]->isAtEnd())) {
     $Loop[0]->moveTime(10);
-    $Loop[1]->moveTime(10);
-    $Loop[2]->moveTime(10);
+    $distdiff[0][$Loop[0]->time() . '000'] = 0;
 
-
-    $distdiff0[$Loop[0]->time() . '000'] = 0;
-    $distdiff1[$Loop[0]->time() . '000'] = 1000 * ($Loop[1]->distance() / $distCoef[1] - $Loop[0]->distance());
-    $distdiff2[$Loop[0]->time() . '000'] = 1000 * ($Loop[2]->distance() / $distCoef[2] - $Loop[0]->distance());
+    for ($i = 1; $i <= $activityCount; $i++) {
+        $Loop[$i]->moveTime(10);
+        if (!$Loop[$i]->isAtEnd()) $distdiff[$i][$Loop[0]->time() . '000'] = 1000 * ($Loop[$i]->distance() / $distCoef[$i] - $Loop[0]->distance());
+    }
 }
 
-$Loop[0]->reset();
-$Loop[1]->reset();
-$Loop[2]->reset();
+for ($i = 0; $i <= $activityCount; $i++) {
+    $Loop[$i]->reset();
+}
 
 $distance = 0;
 while (!($Loop[1]->isAtEnd())) {
-    $distance += 0.1;
+    $distance += 0.05;
     $Loop[0]->moveToDistance($distance);
-    $Loop[1]->moveToDistance($distance * $distCoef[1]);
-    $Loop[2]->moveToDistance($distance * $distCoef[2]);
-
-
     $timediff0[number_format($Loop[0]->distance(), 1)] = '0';
-    $timediff1[number_format($Loop[0]->distance(), 1)] = ($Loop[1]->time() - $Loop[0]->time()) . '000';
-    $timediff2[number_format($Loop[0]->distance(), 1)] = ($Loop[2]->time() - $Loop[0]->time()) . '000';
+
+    for ($i = 1; $i <= $activityCount; $i++) {
+        $Loop[$i]->moveToDistance($distance * $distCoef[$i]);
+        if (!$Loop[$i]->isAtEnd()) $timediff[$i][number_format($Loop[0]->distance(), 1)] = ($Loop[$i]->time() - $Loop[0]->time()) . '000';
+    }
 }
 //var_dump($timediff);exit;
 
 $Plot = new Plot('diff-' . $data['id'], 600, 190);
-$Plot->Data[] = array('label' => 'Virtual racer @' . $vRacerPace, 'color' => '#009900', 'data' => $distdiff0);
-$Plot->Data[] = array('label' => 'Distance 1 vs ' . $vRacerPace, 'color' => '#000099', 'data' => $distdiff1);
-$Plot->Data[] = array('label' => 'Distance 2 vs ' . $vRacerPace, 'color' => '#aa0000', 'data' => $distdiff2);
+$Plot->Data[] = array('label' => 'Virtual racer @' . $vRacerPace, 'data' => $distdiff[0]);
+for ($i = 1; $i <= $activityCount; $i++) {
+    $Loop[$i]->reset();
+    $Plot->Data[] = array('label' => 'Distance ' . $i . ' vs ' . $vRacerPace, 'data' => $distdiff[$i]);
+}
 //$Plot->showPoints();
 $Plot->setXAxisAsTime();
 $Plot->setXAxisTimeFormat("%h:%M:%S");
@@ -96,9 +100,11 @@ $Plot->outputDiv();
 $Plot->outputJavaScript();
 
 $Plot = new IntervalPlot('diff-time-' . $data['id'], 600, 190);
-$Plot->Data[] = array('label' => 'Virtual racer @' . $vRacerPace, 'color' => '#009900', 'data' => $timediff0);
-$Plot->Data[] = array('label' => 'Time 1 vs ' . $vRacerPace, 'color' => '#000099', 'data' => $timediff1);
-$Plot->Data[] = array('label' => 'Time 2 vs ' . $vRacerPace, 'color' => '#aa0000', 'data' => $timediff2);
+$Plot->Data[] = array('label' => 'Virtual racer @' . $vRacerPace, 'data' => $timediff0);
+for ($i = 1; $i <= $activityCount; $i++) {
+    $Loop[$i]->reset();
+    $Plot->Data[] = array('label' => 'Time ' . $i . ' vs ' . $vRacerPace, 'data' => $timediff[$i]);
+}
 //$Plot->showPoints();
 
 //$Plot->setYAxisAsTime();
